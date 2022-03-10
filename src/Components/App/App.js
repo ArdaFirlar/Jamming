b4b80e796d4aa23da76f06e5210e504cf370639f
Arda Firlar <ardafirlar@gmail.com>
2022-03-11 00:29:44 +0300

    third stage

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -12,6 +12,16 @@ class App extends React.Component {
         playlistName: 'My Playlist',
         playlistTracks: [{name: 'playlistName1', artist: 'playlistArtist1', album: 'playlistAlbum2', id: 4}, {name: 'playlistName2', artist: 'playlistArtist2', album: 'playlistAlbum2', id: 5}, {name: 'playlistName3', artist: 'playlistArtist3', album: 'playlistAlbum3', id: 5}]
       }
+      this.addTrack = this.addTrack.bind(this);
+  }
+  addTrack(track){
+    let tracks = this.state.playlistTracks;
+    if(tracks.find(savedTrack => savedTrack.id === track.id)){
+      return;
+    } else{
+      tracks.push(track);
+    }
+    this.setState({Playlist: track});
   }
   render () {
     return(
@@ -20,7 +30,8 @@ class App extends React.Component {
       <div className="App">
         <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults = {this.state.searchResults} />
+            <SearchResults searchResults = {this.state.searchResults}
+                            onAdd = {this.addTrack} />
             <Playlist playlistName = {this.state.playlistName} 
                       playlistTracks = {this.state.playlistTracks} />
           </div>
